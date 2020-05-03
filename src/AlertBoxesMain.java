0053af76d35376c489a56270d477f035d8e073c1
obicco <oskar.bich@gmail.com>
2020-05-03 21:13:30 +0200

    ConfirmBox

--- src/AlertBoxesMain.java
+++ src/AlertBoxesMain.java
@@ -1,13 +1,16 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
 
 public class AlertBoxesMain extends Application{
 
     Stage window;
-    Button button;
+    Button buttonAlert;
+    Button buttonConfirm;
 
     public static void main(String[] args) { launch(args); }
 
@@ -16,11 +19,18 @@ public class AlertBoxesMain extends Application{
         window = primaryStage;
         window.setTitle("Alert Box App");
 
-        button = new Button("Click me");
-        button.setOnAction(e -> AlertBox.display("Alert Box", "Be careful, ALERT BOX!"));
+        buttonAlert = new Button("Alert");
+        buttonAlert.setOnAction(e -> AlertBox.display("Alert Box", "Be careful, ALERT BOX!"));
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        buttonConfirm = new Button("Confirm");
+        buttonConfirm.setOnAction(e -> {
+            boolean result = ConfirmBox.display("Title of Window", "Are you sure?");
+            System.out.println(result);
+        });
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(buttonAlert, buttonConfirm);
+        layout.setAlignment(Pos.CENTER);
         Scene scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
