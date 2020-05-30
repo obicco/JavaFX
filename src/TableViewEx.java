73dd9999619bc650e77679d3fac1d209ee184b9b
obicco <oskar.bich@gmail.com>
2020-05-30 20:35:36 +0200

    TableView Example

--- src/TableViewEx.java
+++ src/TableViewEx.java
@@ -4,8 +4,8 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
@@ -15,6 +15,7 @@ import javafx.stage.Stage;
 public class TableViewEx extends Application {
 
     Stage window;
+    TableView<Product> table;
 
     public static void main(String[] args) { launch(args); }
 
@@ -23,11 +24,41 @@ public class TableViewEx extends Application {
         window = primaryStage;
         window.setTitle("JavaFX - TableView");
 
-        VBox vBox = new VBox();
-        vBox.getChildren().addAll();
+        //Name column
+        TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setMinWidth(200);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name")); //use data for column
 
-        Scene scene = new Scene(vBox, 300, 250);
+        //Price column
+        TableColumn<Product, Double> priceColumn = new TableColumn<>("Price");
+        priceColumn.setMinWidth(100);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price")); //use data for column
+
+        //Name column
+        TableColumn<Product, Integer> quantityColumn = new TableColumn<>("Quantity");
+        quantityColumn.setMinWidth(100);
+        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity")); //use data for column
+
+        table = new TableView<>();
+        table.setItems(getProduct());
+        table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(table);
+
+        Scene scene = new Scene(vBox);
         window.setScene(scene);
         window.show();
+    }
+
+    //Get all of the products
+    public ObservableList<Product> getProduct() {
+        ObservableList<Product> products = FXCollections.observableArrayList();
+        products.add(new Product("Laptop", 859.00, 20));
+        products.add(new Product("Router", 139.00, 13));
+        products.add(new Product("Case", 79.99, 16));
+        products.add(new Product("Mother Board", 129.00, 23));
+        products.add(new Product("GPU", 479.00, 35));
+        return products;
     }
 }
